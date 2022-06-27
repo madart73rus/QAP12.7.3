6042a88ee9fc1e71f75f9d2dbc7b66c8bdfea2a3
madart73rus <98251927+madart73rus@users.noreply.github.com>
2022-06-27 19:43:12 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 money = float(input('money='))
 per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
-deposit = list(map(lambda x: x*money/100, per_cent.values()))
-deposit = list(map(int, deposit))
+deposit = list(map(int,list(map(lambda x: x*money/100, per_cent.values()))))
 print('Возможные накопленные средства за год вклада в каждом из банков', deposit)
 print('Максимальная сумма, которую вы можете заработать', max(deposit))
